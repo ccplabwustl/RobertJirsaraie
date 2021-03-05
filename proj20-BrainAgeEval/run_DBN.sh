bba8ebb90c1ea3d2ac4b91e1c6a428fc5dfc64bd
Robert Jirsaraie <robertjirsaraie21@gmail.com>
2021-03-04 19:09:09 -0600

    Execute Machine Learning Models on the PDS Data

--- proj20-BrainAgeEval/run_DBN.sh
+++ proj20-BrainAgeEval/run_DBN.sh
@@ -21,21 +21,21 @@ fi
 for SCAN in `ls $PROJECT_DIR/mass_skullstrip/*_brain.nii.gz` ; do
 	SUB=`basename $SCAN | cut -d '_' -f1 | cut -d '-' -f2`
 	SES=`basename $SCAN | cut -d '_' -f2 | cut -d '-' -f2`
-	if [[ ! -f ${PROJECT_DIR}/brainages_DBN/${SUB}_${SES}/Subject${SUB}${SES}_T1_BrainAligned.nii.gz ]] ; then	
+	if [[ ! -f ${PROJECT_DIR}/analysis/brainages_DBN/${SUB}_${SES}/Subject${SUB}${SES}_T1_BrainAligned.nii.gz ]] ; then	
 		echo "Extracting the Brain & Standardizing Intensity For SUB: ${SUB} SES: ${SES} "
-		mkdir -p $PROJECT_DIR/brainages_DBN/${SUB}_${SES}
-		OUTPUT=`echo $PROJECT_DIR/brainages_DBN/${SUB}_${SES}/Subject${SUB}${SES}_T1_BrainAligned.nii.gz`
+		mkdir -p $PROJECT_DIR/analysis/brainages_DBN/${SUB}_${SES}
+		OUTPUT=`echo $PROJECT_DIR/analysis/brainages_DBN/${SUB}_${SES}/Subject${SUB}${SES}_T1_BrainAligned.nii.gz`
 		flirt -in $SCAN -ref $SCRIPTS_DIR/mass_1.1.1/data/tpl-MNI152_T1_1mm_brain_LPS_filled.nii.gz -dof 12 -out $OUTPUT
 	fi
 done
-COUNT=`ls ${PROJECT_DIR}/brainages_DBN/*_*/*.nii.gz | wc -l`
-fslmerge -t ${PROJECT_DIR}/brainages_DBN/n${COUNT}_skullstripped.nii.gz `ls ${PROJECT_DIR}/brainages_DBN/*_*/*.nii.gz`
+COUNT=`ls ${PROJECT_DIR}/analysis/brainages_DBN/*_*/*.nii.gz | wc -l`
+fslmerge -t ${PROJECT_DIR}/analysis/brainages_DBN/n${COUNT}_skullstripped.nii.gz `ls ${PROJECT_DIR}/analysis/brainages_DBN/*_*/*.nii.gz`
 
 #########
 ### Split the Input Scans Into 80 Sreenshots
 #########
 
-for INPUT in `echo ${PROJECT_DIR}/brainages_DBN/*/*.nii.gz` ; do
+for INPUT in `echo ${PROJECT_DIR}/analysis/brainages_DBN/*/*.nii.gz` ; do
 	if [[ ! -f $(echo `dirname $INPUT`/*79.jpg) ]] ; then
 		SUB=`basename $INPUT | cut -d '_' -f1 | cut -d '-' -f2`
 		SES=`basename $INPUT | cut -d '_' -f2 | cut -d '-' -f2`
@@ -51,23 +51,23 @@ done
 #########
 
 #python ${SCRIPTS_DIR}/DeepBrainNet/Script/Model_Test.py \
-#	${PROJECT_DIR}/brainages_DBN \
-#	${PROJECT_DIR}/brainages_DBN/output.csv \
+#	${PROJECT_DIR}/analysis/brainages_DBN \
+#	${PROJECT_DIR}/analysis/brainages_DBN/output.csv \
 #	${SCRIPTS_DIR}/DeepBrainNet/CBICA/DBN_model.h5
 
 #LOCAL_DIR=/Users/Jirsaraie/Desktop/Research/proj20-BrainAgeEval
-#rsync -auv rjirsara@login.chpc.wustl.edu:/scratch/rjirsara/proj20-BrainAgeEval/brainages_DBN ${LOCAL_DIR}
-#python3 ${LOCAL_DIR}/DeepBrainNet/Script/Model_Test.py ${LOCAL_DIR}/brainages_DBN ${LOCAL_DIR}/brainages_DBN/output.csv ${LOCAL_DIR}/DeepBrainNet/CBICA/DBN_model.h5
-#rsync -auv ${LOCAL_DIR}/brainages_DBN/output.csv rjirsara@login.chpc.wustl.edu:/scratch/rjirsara/proj20-BrainAgeEval/brainages_DBN
+#rsync -auv rjirsara@login.chpc.wustl.edu:/scratch/rjirsara/proj20-BrainAgeEval/analysis/brainages_DBN ${LOCAL_DIR}
+#python3 ${LOCAL_DIR}/DeepBrainNet/Script/Model_Test.py ${LOCAL_DIR}/analysis/brainages_DBN ${LOCAL_DIR}/analysis/brainages_DBN/output.csv ${LOCAL_DIR}/DeepBrainNet/CBICA/DBN_model.h5
+#rsync -auv ${LOCAL_DIR}/analysis/brainages_DBN/output.csv rjirsara@login.chpc.wustl.edu:/scratch/rjirsara/proj20-BrainAgeEval/analysis/brainages_DBN
 
 #########
 ### Edit Output File To Be Aggregated with the Final Spreadsheet
 #########
 
-NSUBS=`cat ${PROJECT_DIR}/brainages_DBN/output.csv | wc -l` ; NSUBS=`echo $(($NSUBS-1))`
-cat ${PROJECT_DIR}/brainages_DBN/output.csv | tr '_' ',' | tr '/' ',' | cut -d ',' -f1,2,4 > temp.csv
-cat temp.csv | sed s@'ID,Pred'@'sub,ses,brainage_DBN'@g > ${PROJECT_DIR}/brainages_DBN/n${NSUBS}_DeepBrainNet.csv
-rm ${PROJECT_DIR}/brainages_DBN/output.csv temp.csv
+NSUBS=`cat ${PROJECT_DIR}/analysis/brainages_DBN/output.csv | wc -l` ; NSUBS=`echo $(($NSUBS-1))`
+cat ${PROJECT_DIR}/analysis/brainages_DBN/output.csv | tr '_' ',' | tr '/' ',' | cut -d ',' -f1,2,4 > temp.csv
+cat temp.csv | sed s@'ID,Pred'@'sub,ses,brainage_DBN'@g > ${PROJECT_DIR}/analysis/brainages_DBN/n${NSUBS}_DeepBrainNet.csv
+rm ${PROJECT_DIR}/analysis/brainages_DBN/output.csv temp.csv
 
 ########⚡⚡⚡⚡⚡⚡#################################⚡⚡⚡⚡⚡⚡################################⚡⚡⚡⚡⚡⚡#######
 ####           ⚡     ⚡    ⚡   ⚡   ⚡   ⚡  ⚡  ⚡  ⚡    ⚡  ⚡  ⚡  ⚡   ⚡   ⚡   ⚡    ⚡     ⚡         ####
--- proj20-BrainAgeEval/run_GTB.R
+++ proj20-BrainAgeEval/run_GTB.R
@@ -10,16 +10,15 @@ SCRIPTS_DIR="/scratch/rjirsara/RobertJirsaraie/proj20-BrainAgeEval"
 ######
 
 MASTER<-read.csv(paste0(SCRIPTS_DIR,"/GradientTreeBoost/feature-names.csv"),header=F)
-CROSS<-t(MASTER) ; colnames(CROSS)<-t(MASTER)[1,] ; CROSS<-CROSS[-c(1),]
-IDS<-as.data.frame(matrix(NA,nrow=0,ncol=2))
-colnames(IDS)<-c("sub","ses")
+CROSS<-t(MASTER) ; colnames(CROSS)<-t(MASTER)[1,] ; CROSS<-CROSS[-c(1),] ; LONG<-CROSS
 
 ######
 ### Prepare Data By Relabeling Identifiers
 ######
 
-INDEX<-0
-for (FILE in list.files(paste0(PROJECT_DIR,"/freesurf_extract/Data"), full.names=T)){
+FILES<-list.files(paste0(PROJECT_DIR,"/analysis/brainages_GTB"), full.names=T)
+INDEX<-0 ; IDS<-as.data.frame(matrix(NA,nrow=0,ncol=2)) ; colnames(IDS)<-c("sub","ses")
+for (FILE in FILES[lapply(FILES,function(x) length(grep("_long.csv",x,value=FALSE))) == 0]){
 	INDEX=INDEX+1
 	CONTENT<-read.csv(FILE,header=F)
 	TEMP<-merge(MASTER,CONTENT, by="V1", sort = F)
@@ -29,40 +28,76 @@ for (FILE in list.files(paste0(PROJECT_DIR,"/freesurf_extract/Data"), full.names
 	SES<-gsub(".csv","",unlist(strsplit(basename(FILE),"-"))[3])
 	IDS[INDEX,]<-c(SUB,SES)
 }
+CROSS_FULL<-cbind(IDS,CROSS)
+
+INDEX<-0 ; IDS<-as.data.frame(matrix(NA,nrow=0,ncol=2)) ; colnames(IDS)<-c("sub","ses")
+for (FILE in list.files(paste0(PROJECT_DIR,"/analysis/brainages_GTB"), full.names=T,pattern="_long.csv")){
+	INDEX=INDEX+1
+	CONTENT<-read.csv(FILE,header=F)
+	TEMP<-merge(MASTER,CONTENT, by="V1", sort = F)
+	ROW<-as.numeric(t(TEMP)[2,]) ; LONG<-as.data.frame(rbind(LONG,ROW))
+	LONG[1:1118] <- lapply(LONG[1:1118], as.numeric)
+	SUB<-gsub("_ses","",unlist(strsplit(basename(FILE),"-"))[2])
+	SES<-gsub("_long.csv","",unlist(strsplit(basename(FILE),"-"))[3])
+	IDS[INDEX,]<-c(SUB,SES)
+}
+LONG_FULL<-cbind(IDS,LONG)
 
 ######
 ### Split Data By Gender & Save Spreadsheets
 ######
 
-FULL<-cbind(IDS,CROSS) ; FULL$sub<-as.numeric(FULL$sub)
-FINAL<-read.csv(paste0(PROJECT_DIR,"/Analysis/n834_DataFreeze_20201122.csv"))
-FINAL<-FINAL[,c("sub","ses","sex")] ; FINAL<-merge(FINAL,FULL,by=c("sub","ses"))
-FEMALES<-FINAL[which(FINAL$sex==2),] ; MALES<-FINAL[which(FINAL$sex==1),]
-write.csv(FULL,paste0(PROJECT_DIR,"/Analysis/n",nrow(FULL),"_FS-Cross_Glasser_12122020.csv"),row.names=F)
-write.csv(MALES,paste0(PROJECT_DIR,"/Analysis/n",nrow(MALES),"-males_FS-Cross_Glasser_12122020.csv"),row.names=F)
-write.csv(FEMALES,paste0(PROJECT_DIR,"/Analysis/n",nrow(FEMALES),"-females_FS-Cross_Glasser_12122020.csv"),row.names=F)
+FINAL<-read.csv(paste0(PROJECT_DIR,"/analysis/n834_DataFreeze_20201213.csv"))
+CROSS_FULL$sub<-as.numeric(CROSS_FULL$sub) ; FINAL_CROSS<-merge(FINAL,CROSS_FULL,by=c("sub","ses"))
+LONG_FULL$sub<-as.numeric(LONG_FULL$sub) ; FINAL_LONG<-merge(FINAL,LONG_FULL,by=c("sub","ses")) 
+MALES_CROSS<-FINAL_CROSS[which(FINAL_CROSS$sex==1),] ; MALES_LONG<-FINAL_LONG[which(FINAL_LONG$sex==1),]
+FEMALES_CROSS<-FINAL_CROSS[which(FINAL_CROSS$sex==2),] ; FEMALES_LONG<-FINAL_LONG[which(FINAL_LONG$sex==2),]
 
 ######
-### Prepare Data By Relabeling Identifiers
+### Save Datasets
 ######
 
-load("~/Desktop/brainageModels.RData")
-FEMALES<-paste0(PROJECT_DIR,"/Analysis/n",nrow(FEMALES),"-females_FS-Cross_Glasser_12122020.csv")
-MALES<-paste0(PROJECT_DIR,"/Analysis/n",nrow(MALES),"-males_FS-Cross_Glasser_12122020.csv")
-FEMALES_IDS<-FEMALES[,1:2] ; FEMALES<-FEMALES[,-c(1:3)]
-MALES_IDS<-MALES[,1:2] ; MALES<-MALES[,-c(1:3)]
+write.csv(FINAL_LONG,paste0(PROJECT_DIR,"/analysis/n",nrow(LONG_FULL),"_FS-Long_20201231.csv"),row.names=F)
+write.csv(FINAL_CROSS,paste0(PROJECT_DIR,"/analysis/n",nrow(CROSS_FULL),"_FS-Cross_20201231.csv"),row.names=F)
+#write.csv(MALES_CROSS,paste0(PROJECT_DIR,"/analysis/n",nrow(MALES_CROSS),"-MALES_FS-Cross_20201231.csv"),row.names=F)
+#write.csv(FEMALES_CROSS,paste0(PROJECT_DIR,"/analysis/n",nrow(FEMALES_CROSS),"-FEMALES_FS-Cross_20201231.csv"),row.names=F)
+#write.csv(MALES_LONG,paste0(PROJECT_DIR,"/analysis/n",nrow(MALES_LONG),"-MALES_FS-Long_20201231.csv"),row.names=F)
+#write.csv(FEMALES_LONG,paste0(PROJECT_DIR,"/analysis/n",nrow(FEMALES_LONG),"-FEMALES_FS-Long_20201231.csv"),row.names=F)
 
 ######
 ### Run GTB Model and Save Brain Ages Within Master DataFreeze
 ######
 
-FEMALES_IDS$brainage_GTB <- predict(mdl_agepred_female, as.matrix(FEMALES))
-MALES_IDS$brainage_GTB <- predict(mdl_agepred_male, as.matrix(MALES))
-GTB<-rbind(MALES_IDS,FEMALES_IDS)
+load("~/Desktop/brainageModels.RData")
+for (FILE in list.files(paste0(PROJECT_DIR,"/analysis"),full.names=T)[!grepl("DataFreeze",list.files(paste0(PROJECT_DIR,"/analysis"),full.names=T))]){
+	CONTENT<-read.csv(FILE) ; #print(names(CONTENT)[1:20]) ; print(FILE)
+	FEMALES<-CONTENT[which(CONTENT$sex == 2),] ; IDS_FEMALES<-FEMALES[,c("sub","ses")] ; IDS_FEMALES$BRAINAGE<-0 
+	FEMALES<-FEMALES[ , -which(names(FEMALES) %in% c("sub","ses","age","sex","SITE","SCANNER"))] ; colnames(FEMALES)<-NULL
+	MALES<-CONTENT[which(CONTENT$sex == 1),] ; IDS_MALES<-MALES[,c("sub","ses")] ; IDS_MALES$BRAINAGE<-0 
+	MALES<-MALES[ , -which(names(MALES) %in% c("sub","ses","age","sex","SITE","SCANNER"))] ; colnames(MALES)<-NULL
+	IDS_FEMALES$BRAINAGE <- predict(mdl_agepred_female, as.matrix(FEMALES))
+	IDS_MALES$BRAINAGE <- predict(mdl_agepred_male, as.matrix(MALES))
+	GTB<-rbind(IDS_MALES,IDS_FEMALES) 
+	names(GTB)[3]<-paste0("brainage_GTB_",gsub("-",'x',unlist(strsplit(basename(FILE),'_'))[2]))
+	FINAL<-merge(FINAL,GTB,by=c("sub","ses"))
+}
 
-FINAL<-read.csv(paste0(PROJECT_DIR,"/Analysis/n834_DataFreeze_20201122.csv"))
-FINAL<-merge(FINAL,GTB,by=c("sub","ses"),all=T)
-write.csv(FINAL,paste0(PROJECT_DIR,"/Analysis/n834_DataFreeze_20201212.csv"),row.names=F)
+load("~/Desktop/ML-mdl-8-22y_toRobertDeanna_fullbrain_xgboost.RData")
+for (FILE in list.files(paste0(PROJECT_DIR,"/analysis"),full.names=T)[!grepl("DataFreeze",list.files(paste0(PROJECT_DIR,"/analysis"),full.names=T))]){
+	CONTENT<-read.csv(FILE) 
+	FEMALES<-CONTENT[which(CONTENT$sex == 2),] ; IDS_FEMALES<-FEMALES[,c("sub","ses")] ; IDS_FEMALES$BRAINAGE<-0 
+	FEMALES<-FEMALES[ , -which(names(FEMALES) %in% c("sub","ses","age","sex","SITE","SCANNER"))] ; colnames(FEMALES)<-NULL
+	MALES<-CONTENT[which(CONTENT$sex == 1),] ; IDS_MALES<-MALES[,c("sub","ses")] ; IDS_MALES$BRAINAGE<-0 
+	MALES<-MALES[ , -which(names(MALES) %in% c("sub","ses","age","sex","SITE","SCANNER"))] ; colnames(MALES)<-NULL
+	IDS_FEMALES$BRAINAGE <- predict(mdl_agepred_female, as.matrix(FEMALES))
+	IDS_MALES$BRAINAGE <- predict(mdl_agepred_male, as.matrix(MALES))
+	GTB<-rbind(IDS_MALES,IDS_FEMALES) 
+	names(GTB)[3]<-paste0("brainage_rGTB_",gsub("-",'x',unlist(strsplit(basename(FILE),'_'))[2]))
+	FINAL<-merge(FINAL,GTB,by=c("sub","ses"))
+}
+
+#FINAL<-FINAL[ , -which(names(FINAL) %in% c("brainage_GTB"))]
+write.csv(FINAL,paste0(PROJECT_DIR,"/analysis/n834_DataFreeze_20210101.csv"),row.names=F)
 
 ########⚡⚡⚡⚡⚡⚡#################################⚡⚡⚡⚡⚡⚡################################⚡⚡⚡⚡⚡⚡#######
 ####           ⚡     ⚡    ⚡   ⚡   ⚡   ⚡  ⚡  ⚡  ⚡    ⚡  ⚡  ⚡  ⚡   ⚡   ⚡   ⚡    ⚡     ⚡         ####
