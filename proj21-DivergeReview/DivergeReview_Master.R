3b2a8f825d18b1deb1096b87d4b279cb1f2ca43f
Robert Jirsaraie <robertjirsaraie21@gmail.com>
2022-09-20 22:10:28 -0500

    Quantitative Synthesis

--- proj21-DivergeReview/DivergeReview_Master.R
+++ proj21-DivergeReview/DivergeReview_Master.R
@@ -2,8 +2,8 @@
 ######################
 
 lapply(c("ggplot2","tidyverse","dplyr","reshape","effectsize","effsize","lme4","devtools","ggeffects","gamm4","parameters","plyr","lmerTest"), require, character.only=TRUE)
-TIME<-read.csv("https://raw.githubusercontent.com/ccplabwustl/RobertJirsaraie/master/proj21-DivergeReview/n19_MultimodTimeline_20220706.csv")
-META<-read.csv("https://raw.githubusercontent.com/ccplabwustl/RobertJirsaraie/master/proj21-DivergeReview/n20_MultimodLit_20220701.csv")
+META<-read.csv("https://raw.githubusercontent.com/ccplabwustl/RobertJirsaraie/master/proj21-DivergeReview/n20_DivergeReview_20220909.csv")
+TIME<-read.csv("https://raw.githubusercontent.com/ccplabwustl/RobertJirsaraie/master/proj21-DivergeReview/MultiModTimeline_20220706.csv")
 META[which(META$modality=="Multimodal"),"modality"]<-"Multi" ; META$X<-NULL ; META$features_scaled<-NULL 
 META$modality<-factor(gsub(" ","",META$modality)) ; META$domain<-factor(gsub(" ","",META$domain))
 
@@ -25,20 +25,8 @@ META[which(META$domain=="cogntion"),"domain"]<-"cognition"
 META[,c(6,7,10)]<-lapply(META[,c(6,7,10)],as.numeric) ; META[,c(1:5,8,9)]<-lapply(META[,c(1:5,8,9)],factor)
 
 #####
-### Create Figure of Brain Age Timeline
-#####
-
-TIME<-TIME[1:14,]
-TIME$brainage<-TIME$brainage-TIME$mutlimodal
-LONG<-pivot_longer(TIME,cols=c(2:3),names_to="CRITERIA",values_to="COUNT") 
-ggplot(LONG,aes(year))+geom_bar(aes(weight=COUNT,fill=CRITERIA)) + 
-  scale_color_manual(values = c("#3c84c7", "#355e3b")) +
-  scale_fill_manual(values = c("#3c84c7", "#355e3b")) +
-  scale_x_continuous(breaks = round(seq(min(LONG$year), max(LONG$year), by = 1),1)) 
-ggsave("/Users/Jirsaraie/Library/CloudStorage/Box-Box/Research/proj21-DivergeReview/figures/F1_BrainAgeTimeline.pdf",width=13,height=8)
-
-#####
-### Obtain Publicly Shared Data 
+### Obtain and Aggregate Publicly Shared Data 
+### Executed Prior to Developing the "Meta" Spreadsheet
 #####
 
 ### Richard2018 - https://osf.io/gwqmr/
@@ -60,13 +48,54 @@ for (MODEL in 4:14){
   CORRELATION<-NULL
 }
 
-### Dadi2020
+### Dadi2020 - GitHub
 GIT<-"https://raw.githubusercontent.com/RobertJirsaraie/empirical_proxy_measures/master/figure-3/inputs/imaging.csv"
 DATA<-read.csv(GIT)
 DATA$modality<-factor(DATA$modality)
 for (MOD in levels(DATA$modality)){
   print(paste0(MOD,"x",mean(DATA[which(DATA$modality==MOD),"mae"])))
 }
+
+### Engemann2020 - Priviate Email
+#COM<-read.csv("https://raw.githubusercontent.com/ccplabwustl/RobertJirsaraie/master/proj21-DivergeReview/n20_MultimodLit_20220701.csv")
+ENG<-read.csv("/Users/Jirsaraie/Library/CloudStorage/Box-Box/Research/proj21-DivergeReview/aggregate/2020-Engemann.csv") ; INDEX<-0
+for (PHENOTYPE in c("CardioMeasures_bp_sys_mean","mmse_i","psqi","HADS_depression","EmotionalMemory_ObjPr_pca1")){
+  SUBSET<-ENG[which(ENG$cognitive_score==PHENOTYPE),c("estimate","modality")] ; INDEX<-INDEX+1
+  SUBSET$study<-"Engemann2020" ; SUBSET$studyxdomain<-paste0("6_",INDEX) ; SUBSET$Inclusion<-"C"
+  SUBSET[grep(",",SUBSET$modality),"modality"]<-"Multi" ; SUBSET$features_label<-SUBSET$modality
+  SUBSET$features_dim<-c(45069,42826,12557,32512,2243,10314) 
+  SUBSET$accuracy_mae<-c(4.7,5.1,5.2,5.8,6.7,6.0)
+  names(SUBSET)[1]<-"utility_raw"
+  SUBSET$metric<-"stdBetas" 
+  SUBSET$domain<-PHENOTYPE
+  if (INDEX == 1){ 
+    SUBSET$domain<-"BPSystolic"
+  } else if (INDEX == 2){ 
+    SUBSET$domain<-"mild_cog_impair"
+  } else if (INDEX == 3){
+    SUBSET$domain<-"sleepquality"
+  } else if (INDEX == 4){ 
+    SUBSET$domain<-"depression"
+  } else if (INDEX == 5){ 
+    SUBSET$domain<-"cognition"
+  }
+  META<-rbind(META,SUBSET)
+}
+META<-META[-which(META$domain=="TBD"),]
+META<-META[c(1:66,grep("6_",META$studyxdomain),67:199),] ; row.names(META)<-NULL
+
+#####
+### Create Figure of Brain Age Timeline
+#####
+
+TIME<-TIME[1:14,]
+TIME$brainage<-TIME$brainage-TIME$mutlimodal
+LONG<-pivot_longer(TIME,cols=c(2:3),names_to="CRITERIA",values_to="COUNT") 
+ggplot(LONG,aes(year))+geom_bar(aes(weight=COUNT,fill=CRITERIA)) + 
+  scale_color_manual(values = c("#3c84c7", "#355e3b")) +
+  scale_fill_manual(values = c("#3c84c7", "#355e3b")) +
+  scale_x_continuous(breaks = round(seq(min(LONG$year), max(LONG$year), by = 1),1)) 
+ggsave("/Users/Jirsaraie/Library/CloudStorage/Box-Box/Research/proj21-DivergeReview/figures/F1_BrainAgeTimeline.pdf",width=13,height=8)
 
 #####
 ### Convert the Effect Sizes Onto a Standardized Scale
@@ -95,34 +124,28 @@ for (ROW in which(META$metric=="stdBetas")){
   META[ROW,"utility_converted"]<-abs(META[ROW,"utility_raw"])
 }
 
-META<-META[-which(META$study=="Engemann2020"),] ; row.names(DATA)<-NULL #TEMP
 META[,c(6,7,10,11)]<-lapply(META[,c(6,7,10,11)],as.numeric)
 META[,c(1:5,8,9)]<-lapply(META[,c(1:5,8,9)],factor)
+row.names(META)<-NULL  
 
 #####
 ### Create Figure of Model Accuracy
 #####
 
-theme_set(theme_bw(base_size = 16))
-CHECKPOINT<-META
+META$modality<-as.character(META$modality)
+META[which(META$modality=="MRI"),"modality"]<-"T1w"
 META[which(META$modality=="ASL"),"modality"]<-"FUNC"
+META[which(META$modality=="fMRI"),"modality"]<-"FUNC"
 META$study<-factor(META$study,levels=unique(META$study))
 MODALITY_ORDER<-c("Multi","T1w","T2w","DWI","FUNC","MEG")
 META$modality<-factor(META$modality,levels=MODALITY_ORDER)
-#META<-META[!is.na(META$modality),] ; META$modality<-factor(META$modality,levels=MODALITY_ORDER)
-#theme(legend.position="none",aspect.ratio=1/1.25,panel.grid.minor=element_blank(),axis.title=element_blank())
+theme_set(theme_bw(base_size = 16)) ; CHECKPOINT<-META
 
 #Remove Extra Rows For Studies that had more than 1 Developmental Outcome
 for (REMOVE in 2:5){
   META<-META[-grep(paste0("_",REMOVE), META$studyxdomain),]
   row.names(META)<-NULL
   META$studyxdomain<-factor(META$studyxdomain)
-}
-tMETA<-rMETA
-for (REMOVE in 2:5){
-  tMETA<-tMETA[-grep(paste0("_",REMOVE), tMETA$studyxdomain),]
-  row.names(tMETA)<-NULL
-  tMETA$studyxdomain<-factor(tMETA$studyxdomain)
 }
 
 #Min/Max Scale the MAEs and Number of NeuroImaging Features
@@ -156,7 +179,7 @@ ggplot() +
   geom_point(data=F1,aes(x=modality,y=accuracy_mae_scaled,group=study,color=study),size=2,alpha=0.75) +
   geom_line(data=ACCURACY,aes(x=modality,y=accuracy_mae_scaled,group=study),size=0.7,alpha=0.7,color="#000000") + 
   geom_point(data=ACCURACY,aes(x=modality,y=accuracy_mae_scaled),size=3,alpha=0.9,color="#000000") 
-ggsave("/Users/Jirsaraie/Library/CloudStorage/Box-Box/Research/proj21-DivergeReview/figures/F1_Accuracy.pdf",width=8,height=7)
+ggsave("/Users/Jirsaraie/Library/CloudStorage/Box-Box/Research/proj21-DivergeReview/figures/F1A_Accuracy.pdf",width=8,height=7)
 
 #Figure of Accuracy by Dimensonality
 ggplot() +
@@ -174,12 +197,7 @@ model_parameters(lmer(accuracy_mae_scaled ~ features_dim_scaled + (1|study),data
 #####
 
 rMETA<-CHECKPOINT[which(CHECKPOINT$Inclusion=="C"),]
-rMETA[which(rMETA$modality=="ASL"),"modality"]<-"FUNC"
-rMETA$study<-factor(rMETA$study,levels=unique(rMETA$study))
-MODALITY_ORDER<-c("Multi","T1w","T2w","DWI","FUNC")
-rMETA$modality<-factor(rMETA$modality,levels=MODALITY_ORDER)
-rMETA<-rMETA[!is.na(rMETA$modality),] 
-rMETA$modality<-factor(rMETA$modality,levels=MODALITY_ORDER)
+MODALITY_ORDER<-c("Multi","T1w","T2w","DWI","FUNC","MEG")
 
 #Min/Max Scale the MAEs and Number of NeuroImaging Features
 for (STUDY in unique(rMETA$studyxdomain)){
@@ -233,6 +251,7 @@ rMETA[which(rMETA$domain=='bipolar'),"domain"]<-"Bipolar Disorder"
 rMETA[which(rMETA$domain=='cognition'),"domain"]<-"Cognition"
 rMETA[which(rMETA$domain=='education'),"domain"]<-"Education"
 rMETA[which(rMETA$domain=='fitness'),"domain"]<-"Physical Fitness"
+rMETA[which(rMETA$domain=='depression'),"domain"]<-"Depression"
 rMETA[which(rMETA$domain=='mild_cog_impair'),"domain"]<-"Cognitive Impariments"
 rMETA[which(rMETA$domain=='schizophrenia'),"domain"]<-"Schizophrenia"
 rMETA[which(rMETA$domain=='sleepquality'),"domain"]<-"Sleep Quality"
@@ -240,7 +259,7 @@ rMETA[which(rMETA$domain=='subject_cog_impair'),"domain"]<-"Cognitive Complaints
 rMETA[which(rMETA$domain=='telomerlength'),"domain"]<-"Telomere Length"
 rMETA[which(rMETA$domain=='BPSystolic'),"domain"]<-"Blood Pressure"
 rMETA[which(rMETA$domain=='StrokeRisk'),"domain"]<-"Stroke Risk"
-rMETA$domain<-factor(rMETA$domain,levels=c("Alcohol Use","Blood Pressure","Physical Fitness","Sleep Quality","Stroke Risk","Telomere Length","Alzheimer's Disease","Cognitive Complaints","Cognitive Impariments","Cognition","Education","Bipolar Disorder","Schizophrenia"))      
+rMETA$domain<-factor(rMETA$domain,levels=c("Alcohol Use","Blood Pressure","Physical Fitness","Sleep Quality","Stroke Risk","Telomere Length","Alzheimer's Disease","Cognitive Complaints","Cognitive Impariments","Cognition","Education","Depression","Bipolar Disorder","Schizophrenia"))      
 
 F3<-NULL 
 for (STUDY in unique(rMETA$studyxdomain)){
@@ -254,8 +273,9 @@ for (STUDY in unique(rMETA$studyxdomain)){
 ggplot() +
   geom_line(data=F3,aes(x=modality,y=utility_converted,group=domain,color=domain),size=0.75,alpha=0.5) + 
   geom_point(data=F3,aes(x=modality,y=utility_converted,group=domain,color=domain),size=2,alpha=0.5) +
-  facet_wrap(~study)
-ggsave("/Users/Jirsaraie/Library/CloudStorage/Box-Box/Research/proj21-DivergeReview/figures/F4A_UtilityByDomain.pdf",width=12,height=7) 
+  facet_wrap(~study) +
+  theme(legend.position="none",aspect.ratio=1/1.5,panel.grid.minor=element_blank(),axis.title=element_blank()) 
+ggsave("/Users/Jirsaraie/Library/CloudStorage/Box-Box/Research/proj21-DivergeReview/figures/F3_UtilityByDomain.pdf",width=16,height=8) 
 
 #####
 ### Create More Granular Figure of Model Utility Based on Developmental Outcomes
@@ -269,7 +289,7 @@ for (DOMAIN in unique(rMETA$domain)){
   FSUPP<-rbind(FSUPP,ADD)
 }
 
-FSUPP$domain<-factor(FSUPP$domain,levels=c("Alcohol Use","Blood Pressure","Physical Fitness","Sleep Quality","Stroke Risk","Telomere Length","Alzheimer's Disease","Cognitive Complaints","Cognitive Impariments","Cognition","Education","Bipolar Disorder","Schizophrenia"))      
+FSUPP$domain<-factor(FSUPP$domain,levels=c("Alcohol Use","Blood Pressure","Physical Fitness","Sleep Quality","Stroke Risk","Telomere Length","Alzheimer's Disease","Cognitive Complaints","Cognitive Impariments","Cognition","Education","Depression","Bipolar Disorder","Schizophrenia"))      
 fUTILITY<-ddply(FSUPP,"modality",summarize,utility_converted=mean(utility_converted),utility_converted_scaled=mean(utility_converted_scaled),domain="Aggregate") 
 ggplot() +
   geom_line(data=FSUPP,aes(x=modality,y=utility_converted,group=domain,color=domain),size=0.5,alpha=0.5) + 
@@ -279,8 +299,8 @@ ggplot() +
 ggsave("/Users/Jirsaraie/Library/CloudStorage/Box-Box/Research/proj21-DivergeReview/figures/F4A_UtilityByDomain.pdf",width=8,height=7) 
 
 ggplot() +
-  geom_smooth(data=rMETA,aes(x=features_dim_scaled,y=utility_converted,group=domain,color=domain),method='lm',size=1.25,alpha=0.1,fullrange=F,se=F) + 
-  geom_smooth(data=rMETA,aes(x=features_dim_scaled,y=utility_converted),color="#000000",method='lm',size=2.5,alpha=0.75,fullrange=F,se=F) 
+  geom_smooth(data=rMETA,aes(x=features_dim_scaled,y=utility_converted,group=domain,color=domain),method='lm',size=0.5,alpha=0.5,fullrange=F,se=F) + 
+  geom_smooth(data=rMETA,aes(x=features_dim_scaled,y=utility_converted),color="#000000",method='lm',size=2,alpha=0.75,fullrange=F,se=F) 
 ggsave("/Users/Jirsaraie/Library/CloudStorage/Box-Box/Research/proj21-DivergeReview/figures/F4B_UtilityByDomain.pdf",width=8,height=7) 
 
 #Statistical Analyses
@@ -310,9 +330,12 @@ for (DOMAIN in unique(rMETA$domain)){
 
 ggplot() +
   geom_point(data=rMETA,aes(x=accuracy_mae_scaled,y=utility_converted,color=study),size=1.25,alpha=0.6) +
-  geom_smooth(data=rMETA,aes(x=accuracy_mae_scaled,y=utility_converted),color="#000000",method='lm',size=2.5,alpha=0.75,se=F) + 
-  geom_smooth(data=rMETA,aes(x=accuracy_mae_scaled,y=utility_converted,group=study,color=study),method='lm',size=1.25,alpha=0.6,se=F) 
-ggsave("/Users/Jirsaraie/Library/CloudStorage/Box-Box/Research/proj21-DivergeReview/figures/F3A_Assocation.pdf",width=9,height=7)
+  geom_smooth(data=rMETA,aes(x=accuracy_mae_scaled,y=utility_converted),color="#000000",method='lm',size=2,alpha=0.75,se=F) + 
+  geom_smooth(data=rMETA,aes(x=accuracy_mae_scaled,y=utility_converted,group=study,color=study),method='lm',size=0.5,alpha=0.5,se=F) 
+ggsave("/Users/Jirsaraie/Library/CloudStorage/Box-Box/Research/proj21-DivergeReview/figures/F5_Assocation.pdf",width=11,height=8) 
+
+  geom_smooth(data=rMETA,aes(x=features_dim_scaled,y=utility_converted,group=domain,color=domain),method='lm',size=0.5,alpha=0.5,fullrange=F,se=F) + 
+  geom_smooth(data=rMETA,aes(x=features_dim_scaled,y=utility_converted),color="#000000",method='lm',size=2,alpha=0.75,fullrange=F,se=F) 
 
 #Statistical Analyses
 model_parameters(lmer(utility_converted ~ accuracy_mae_scaled + (1|study/studyxdomain),data=rMETA, REML=TRUE), standardize="basic")
@@ -327,82 +350,6 @@ for (STUDY in unique(rMETA$study)){
     print(model_parameters(lmer(utility_converted ~ accuracy_mae_scaled + (1|studyxdomain),data=TEMP, REML=TRUE), standardize="basic"))
   }
 }
-
-########⚡⚡⚡⚡⚡⚡#################################⚡⚡⚡⚡⚡⚡################################⚡⚡⚡⚡⚡⚡#######
-####           ⚡     ⚡    ⚡   ⚡   ⚡   ⚡  ⚡  ⚡  ⚡    ⚡  ⚡  ⚡  ⚡   ⚡   ⚡   ⚡    ⚡     ⚡         ####
-########⚡⚡⚡⚡⚡⚡#################################⚡⚡⚡⚡⚡⚡################################⚡⚡⚡⚡⚡⚡#######
-
-###F2: Scatterplots of Paradox By Dimesionality - Collective
-LONG<-pivot_longer(META,cols=c(11,14),names_to="DOMAIN",values_to="POINTS") 
-UTILITY<-ggpredict(lmer(utility_converted ~ features_dim_scaled + (1|study), data = META), terms = "features_dim_scaled")
-ACCURACY<-ggpredict(lmer(accuracy_mae_scaled ~ features_dim_scaled + (1|study), data = META), terms = "features_dim_scaled")
-STACK1<-as.data.frame(cbind("ACCURACY",ACCURACY$x,ACCURACY$predicted)) 
-STACK2<-as.data.frame(cbind("UTILITY",UTILITY$x,UTILITY$predicted)) 
-PARADOX<-NULL ; PARADOX<-rbind(STACK1,STACK2)
-names(PARADOX)<-c("DOMAIN","X","Y") ; PARADOX[,c("X","Y")]<-lapply(PARADOX[,c("X","Y")], as.numeric)
-ggplot() +
-  geom_point(data=LONG,aes(x=features_dim_scaled,y=POINTS,group=DOMAIN,color=DOMAIN),size=2,alpha=0.7) + 
-  geom_smooth(data=PARADOX,aes(x=X,y=Y,group=DOMAIN,color=DOMAIN),method='lm',size=3,alpha=0.7,fullrange=F,se=F) + 
-  scale_fill_manual(values=c("#777B7B","#777B7B","#800000","#800000")) +
-  scale_color_manual(values=c("#777B7B","#777B7B","#800000","#800000")) +
-  facet_wrap(~domain_cluster) +
-  theme(legend.position="none",aspect.ratio=1/1.25,panel.grid.minor=element_blank(),axis.title=element_blank()) 
-ggsave("/Users/Jirsaraie/Library/CloudStorage/Box-Box/Research/proj21-DivergeReview/figures/F2_Collective.pdf",width=7.4,height=6.8)
-
-###F2: Scatterplots of Paradox By Dimesionality - Modality Specific
-PARADOX<-NULL ; LONG<-NULL
-for (MODE in c("anat", "dwi", "func")){
-  TEMP<-META[which(META$modality == MODE),c(2,11,13,14)]
-  TEMP1<-pivot_longer(TEMP,cols=c(2,4),names_to="DOMAIN",values_to="POINTS") 
-  TEMP1[,"MODE"]<-MODE ; LONG<-rbind(LONG,TEMP1)
-  UTILITY<-ggpredict(lmer(utility_converted ~ features_dim_scaled + (1|study), data = TEMP), terms = "features_dim_scaled")
-  ACCURACY<-ggpredict(lmer(accuracy_mae_scaled ~ features_dim_scaled + (1|study), data = TEMP), terms = "features_dim_scaled")
-  STACK1<-as.data.frame(cbind(MODE,"ACCURACY",ACCURACY$x,ACCURACY$predicted)) 
-  STACK2<-as.data.frame(cbind(MODE,"UTILITY",UTILITY$x,UTILITY$predicted)) 
-  PARADOX<-rbind(PARADOX,STACK1,STACK2)
-}
-names(PARADOX)<-c("MODE","DOMAIN","X","Y") ; PARADOX[,c("X","Y")]<-lapply(PARADOX[,c("X","Y")], as.numeric)
-ggplot() +
-  geom_point(data=LONG,aes(x=features_dim_scaled,y=POINTS,group=DOMAIN,color=DOMAIN),size=2,alpha=1) + 
-  geom_smooth(data=PARADOX,aes(x=X,y=Y,group=DOMAIN,color=DOMAIN),method='lm',size=3,alpha=1,fullrange=F,se=F) + 
-  scale_fill_manual(values=c("#777B7B","#777B7B","#A30000","#A30000")) +
-  scale_color_manual(values=c("#777B7B","#777B7B","#A30000","#A30000")) +
-  facet_wrap(~MODE,ncol=1) + 
-  theme(legend.position="none",aspect.ratio=1/2.25,panel.grid.minor=element_blank(),axis.title=element_blank()) 
-ggsave(paste0("/Users/Jirsaraie/Library/CloudStorage/Box-Box/Research/proj21-DivergeReview/figures/F2_ModalitySpecific.pdf"),width=7.4,height=6.8)
-
-###F3: Histograms Regarding the Accuracy/Utility Paradox - Collective
-GGPREDICT<-ggpredict(lmer(utility_converted ~ accuracy_mae_scaled + (1|studyxdomain), data = META), terms = "accuracy_mae_scaled")
-ggplot() +
-  geom_point(data=META,aes(x=accuracy_mae_scaled,y=utility_converted,group=study,color=study),size=2,alpha=0.7) + 
-  geom_smooth(data=GGPREDICT,aes(x=x,y=predicted),color="#000000",method='lm',size=3,alpha=0.9,fullrange=F,se=F) + 
-  scale_fill_manual(values=c("#d11141","#ffc425","#00b159","#00aedb","#f37735")) +
-  scale_color_manual(values=c("#d11141","#ffc425","#00b159","#00aedb","#f37735")) +
-  facet_wrap(~domain_cluster) +
-  theme(legend.position="none",aspect.ratio=1/1.25,panel.grid.minor=element_blank(),axis.title=element_blank()) 
-ggsave("/Users/Jirsaraie/Library/CloudStorage/Box-Box/Research/proj21-DivergeReview/figures/F3_Collective-scaledMAE.pdf",width=7.4,height=6.8)
-
-###F3: Scatterplots of Paradox By Dimesionality - Modality Specific
-PARADOX<-NULL ; LONG<-NULL
-for (MODE in c("anat", "dwi", "func")){
-  TEMP<-META[which(META$modality == MODE),c(1,2,7,11,13,14)]
-  GGPREDICT<-ggpredict(lmer(utility_converted ~ accuracy_mae_scaled + (1|study), data = TEMP), terms = "accuracy_mae_scaled")
-  STACK<-as.data.frame(cbind(MODE,GGPREDICT$x,GGPREDICT$predicted)) 
-  PARADOX<-rbind(PARADOX,STACK)
-}
-names(PARADOX)<-c("MODE","X","Y") ; PARADOX[,c("X","Y")]<-lapply(PARADOX[,c("X","Y")], as.numeric)
-sMETA<-META[which(META$modality==c("anat")),] 
-fMETA<-META[which(META$modality==c("func")),] 
-dMETA<-META[which(META$modality==c("dwi")),]
-mMETA<-rbind(sMETA,fMETA,dMETA) ; names(mMETA)[which(names(mMETA)=="modality")]<-"MODE"
-ggplot() +
-  geom_point(data=mMETA,aes(x=accuracy_mae_scaled,y=utility_converted,group=MODE,color=study),size=2,alpha=1) + 
-  geom_smooth(data=PARADOX,aes(x=X,y=Y),method='lm',color="#000000",size=3,alpha=1,fullrange=F,se=F) + 
-  scale_fill_manual(values=c("#d11141","#ffc425","#00b159","#00aedb","#f37735")) +
-  scale_color_manual(values=c("#d11141","#ffc425","#00b159","#00aedb","#f37735")) +
-  facet_wrap(~MODE,ncol=1) + 
-  theme(legend.position="none",aspect.ratio=1/2.25,panel.grid.minor=element_blank(),axis.title=element_blank()) 
-ggsave(paste0("/Users/Jirsaraie/Library/CloudStorage/Box-Box/Research/proj21-DivergeReview/figures/F3_ModalitySpecific-scaledMAE.pdf"),width=7.4,height=6.8)
 
 ########⚡⚡⚡⚡⚡⚡#################################⚡⚡⚡⚡⚡⚡################################⚡⚡⚡⚡⚡⚡#######
 ####           ⚡     ⚡    ⚡   ⚡   ⚡   ⚡  ⚡  ⚡  ⚡    ⚡  ⚡  ⚡  ⚡   ⚡   ⚡   ⚡    ⚡     ⚡         ####
